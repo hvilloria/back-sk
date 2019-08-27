999277112278765d248979ddefa86c9ee49b9216
Hosward <hosward.avc@gmail.com>
2019-08-26 22:50:10 -0300

    adds a not null constraint into the product migration

--- db/migrate/20190827012850_create_products.rb
+++ db/migrate/20190827012850_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.float :price
+      t.string :name, null: false
+      t.float :price, null: false
       t.references :category, foreign_key: true
       t.integer :presentation
 
