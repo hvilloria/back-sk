e1a4d7276ef37ab148d7d75de5df76010134f375
Hosward <hosward.avc@gmail.com>
2019-08-26 22:47:56 -0300

    adds the association to a product model and tests it

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to have_many(:products) }
 end
