d200f617d4c926d475cf2714f9222dfa31426d6c
Edka <jedkaryd@gmail.com>
2019-08-30 00:12:25 -0300

    create update product endpoint!

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  class ProductsController < ApplicationController
+    def update
+      @product = Product.find(params[:id])
+      @product.assign_attributes(product_permitted_params)
+      return bad_request(@product.errors) unless @product.save
+
+      render json: @product, status: :ok
+    end
+
+    private
+
+    def product_permitted_params
+      params.require(:product).permit(:name, :price, :status)
+    end
+  end
+end
