c06133794df269a30cdedd3990ce836c171e639b
Hosward <hosward.avc@gmail.com>
2019-10-02 22:23:24 -0300

    adds a method in category model to update the product status if the category status was updated

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,20 +6,72 @@ RSpec.describe Category, type: :model do
   it { is_expected.to have_many(:products) }
   it { is_expected.to have_one(:discount) }
 
-  before do
-    create_list(:category, 4)
-    create(:category, status: 'inactive')
-  end
+  context 'using scopes' do
+    before do
+      create_list(:category, 4)
+      create(:category, status: 'inactive')
+    end
 
-  context 'when using active_ones scope' do
-    it 'shows only the active categories' do
-      expect(described_class.active_ones.size).to eq(4)
+    context 'when using active_ones scope' do
+      it 'shows only the active categories' do
+        expect(described_class.active_ones.size).to eq(4)
+      end
+    end
+
+    context 'when using inactive_ones scope' do
+      it 'shows only the inactive categories' do
+        expect(described_class.inactive_ones.size).to eq(1)
+      end
     end
   end
 
-  context 'when using inactive_ones scope' do
-    it 'shows only the inactive categories' do
-      expect(described_class.inactive_ones.size).to eq(1)
+  describe '#change_products_status' do
+    context 'when the category is active' do
+      let!(:category) { create(:category, status: 'active') }
+
+      context 'and gets inactivated' do
+        context 'and products are active' do
+          let!(:product) { create(:product, status: 'active', category: category) }
+
+          it 'inactivates all its products' do
+            category.inactive!
+            expect(product.reload.status).to eq('inactive')
+          end
+        end
+
+        context 'and products are inactive' do
+          let!(:product) { create(:product, status: 'inactive', category: category) }
+
+          it 'does not change the products status' do
+            category.inactive!
+            expect(product.reload.status).to eq('inactive')
+          end
+        end
+      end
+    end
+
+    context 'when the category is inactive' do
+      let!(:category) { create(:category, status: 'inactive') }
+
+      context 'and gets activated' do
+        context 'and products are active' do
+          let!(:product) { create(:product, status: 'active', category: category) }
+
+          it 'does not change the products status' do
+            category.active!
+            expect(product.reload.status).to eq('active')
+          end
+        end
+
+        context 'and products are inactive' do
+          let!(:product) { create(:product, status: 'inactive', category: category) }
+
+          it 'activates its products' do
+            category.active!
+            expect(product.reload.status).to eq('active')
+          end
+        end
+      end
     end
   end
 end
