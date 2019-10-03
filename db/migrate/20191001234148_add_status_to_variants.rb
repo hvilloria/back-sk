36261b46b765dc35409584c8b67ad251e6cdd425
Edka <jedkaryd@gmail.com>
2019-10-02 21:18:28 -0300

    allow status as nested param for products creation

--- db/migrate/20191001234148_add_status_to_variants.rb
+++ db/migrate/20191001234148_add_status_to_variants.rb
@@ -1,5 +1,5 @@
 class AddStatusToVariants < ActiveRecord::Migration[5.2]
   def change
-    add_column :variants, :status, :string, default: 'active'
+    add_column :variants, :status, :string, null: false, default: 'active'
   end
 end
