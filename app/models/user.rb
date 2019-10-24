0d67c5742e5fd07fd591276d09cbe22b1dd3ee5a
Edka <jedkaryd@gmail.com>
2019-10-23 23:38:42 -0300

    add devise for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable,
+         :validatable, :confirmable
+
+  include DeviseTokenAuth::Concerns::User
+
   validates :name, presence: true
 end
---
+++ db/migrate/20191024020107_devise_token_auth_create_users.rb
@@ -0,0 +1,29 @@
+class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_table(:users) do |t|
+      ## Required
+      t.string :provider, :null => false, :default => "email"
+      t.string :uid, :null => false, :default => ""
+
+      ## Database authenticatable
+      t.string :encrypted_password, null: false, default: ''
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.boolean  :allow_password_change, default: false
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+
+      ## Tokens
+      t.json :tokens
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
+  end
+end
