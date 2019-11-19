cf6ff47b80d9cf83ccac092923ad5b07f9934f11
Hosward <hosward.avc@gmail.com>
2019-11-18 22:12:00 -0300

    fixes created_at field in the order model, new spec was created to test it with new format

--- spec/serializers/order_serializer_spec.rb
+++ spec/serializers/order_serializer_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe Api::OrderSerializer, type: :serializer do
   subject { JSON.parse(serialization.to_json) }
 
   ORDER_SERIALIZER_FIELDS = %w[tracking_id service_type shipping_cost total
-                               notes created_at payment_type client_name
+                               notes payment_type client_name
                                client_phone_number].freeze
 
   ORDER_SERIALIZER_FIELDS.each do |field|
     it "serializes the #{field.humanize(capitalize: false)} field" do
       expect(serializer.serializable_hash[field.to_sym]).to eq order.send(field)
     end
+  end
+
+  it 'serializes the created_at field with more readable format' do
+    expect(serializer.serializable_hash[:created_at].to_date).to eq order.created_at.to_date
   end
 end
